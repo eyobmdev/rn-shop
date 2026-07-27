74ad26782bc4cd2597366bb169859edf53b412d0
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-28 02:45:35 +0300

    platform specifc code

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
 
 export default function App() {
-
-  return <ViewImageScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppText>
+        Open up App.js to start working on your app!{" "}
+        <MaterialCommunityIcons name="heart" size={20} color="red" />
+      </AppText>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
