920c5a953a61b0fcefd7fb94df2821cb57262b1c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-28 02:59:15 +0300

    refactor

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import { SafeAreaView, StyleSheet, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/components/AppText/AppText";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <AppText>
-        Open up App.js to start working on your app!{" "}
+        Open up App.js to start working on your app!!!{" "}
         <MaterialCommunityIcons name="heart" size={20} color="red" />
       </AppText>
     </View>
--- app/components/AppText/AppText.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text, Platform } from "react-native";
-import styles from "./styles";
-
-function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
-}
-
-export default AppText;
