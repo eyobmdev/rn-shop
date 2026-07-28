67e0a03061aa018dce00e7c469a91a7582d38b4e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-28 03:37:17 +0300

    implemetn the welcome screen module

--- App.js
+++ App.js
@@ -4,14 +4,8 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <AppText>
-        Open up App.js to start working on your app!!!{" "}
-        <MaterialCommunityIcons name="heart" size={20} color="red" />
-      </AppText>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,30 @@
 import React from "react";
 import { View, Text, ImageBackground, StyleSheet, Image } from "react-native";
 
+import AppButton from "../components/AppButton";
+
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       source={require("../assets/background.jpg")}
       style={styles.background}
     >
       <View style={styles.logoContainer}>
         <Image source={require("../assets/logo-red.png")} style={styles.logo} />
-        <Text>Sell Anything, Buy Anything</Text>
+        <Text style={styles.tagline}>Sell Anything, Buy Anything</Text>
       </View>
-      <View style={styles.loginButton} />
-      <View style={styles.registerButton} />
+      <View style={styles.buttonsContainer}>
+        <AppButton
+          title="Login"
+          onPress={() => console.log("Button pressed")}
+        />
+        <AppButton
+          title="Register"
+          color="secondary"
+          onPress={() => console.log("Button pressed")}
+        />
+      </View>
     </ImageBackground>
   );
 }
@@ -32,15 +44,14 @@ const styles = StyleSheet.create({
     top: 70,
     alignItems: "center",
   },
-  loginButton: {
+  buttonsContainer: {
     width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
+    padding: 20,
   },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
+  tagline: {
+    fontSize: 20,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
 });
 
