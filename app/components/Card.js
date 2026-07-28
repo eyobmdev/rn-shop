91e2f9b99d97fc0277f9a031e0c1ca0a617365ba
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-28 04:22:17 +0300

    created card

---
+++ app/components/Card.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+import AppText from "./AppText";
+import colors from "../configs/colors";
+
+function Card({ title, subtitle, image }) {
+  return (
+    <View style={styles.card}>
+      <Image source={image} style={styles.image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subtitle}>{subtitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 15,
+    overflow: "hidden",
+    backgroundColor: colors.white,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    marginBottom: 7,
+  },
+  subtitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
+});
+
+export default Card;
