9f24aac71c278ad25f395c612fa9a92cec21d0fd
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-30 00:00:55 +0300

    landing detail

--- app/screens/ListingDetailScreen.js
+++ app/screens/ListingDetailScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, StyleSheet, Image } from "react-native";
 import AppText from "../components/AppText";
 import colors from "../configs/colors";
+import ListItem from "../components/ListItem";
 
 function ListingDetailScreen(props) {
   return (
@@ -10,6 +11,13 @@ function ListingDetailScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title="Mosh Hamedani"
+            subtitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -32,6 +40,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 20,
     marginVertical: 10,
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
