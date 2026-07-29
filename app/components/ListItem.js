786c5f348a1e85cd5991e21cd8edaf201a636d27
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-30 00:01:21 +0300

    add user display component

---
+++ app/components/ListItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import AppText from "./AppText";
+import colors from "../configs/colors";
+
+function ListItem({ image, title, subtitle }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <View style={styles.detailContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subtitle}>{subtitle}</AppText>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: "50%",
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  subtitle: {
+    color: colors.medium,
+  },
+  detailContainer: {
+    marginLeft: 10,
+  },
+});
+
+export default ListItem;
