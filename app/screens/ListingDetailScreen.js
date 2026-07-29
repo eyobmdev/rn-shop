757f129c995f699a3582449a413caad62fc6308e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-07-29 23:17:33 +0300

    create item detail page

---
+++ app/screens/ListingDetailScreen.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import AppText from "../components/AppText";
+import colors from "../configs/colors";
+
+function ListingDetailScreen(props) {
+  return (
+    <View>
+      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+});
+
+export default ListingDetailScreen;
