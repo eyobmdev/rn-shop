264ad62f1cc579aec315bed1f80f462d1e29ba72
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-04 01:58:09 +0300

    refactor: format Screen component for improved readability

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -2,7 +2,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
 
 function Screen({ children }) {
-  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.screen} edges={["top", "bottom"]}>
+      {children}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
