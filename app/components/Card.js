8cce697eb4e809bc222e88fbab8e57d5740d4f86
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-04 03:38:17 +0300

    feat: adjust Card component styles for improved layout

--- app/components/Card.js
+++ app/components/Card.js
@@ -21,6 +21,7 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     overflow: "hidden",
     backgroundColor: colors.white,
+    marginVertical: 10,
   },
   image: {
     width: "100%",
