7f04e086dae149f0e3e3f6900185a58a6d1d5ca7
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-07 03:09:19 +0300

    feat: add AppPicker component for item selection with modal

---
+++ app/components/AppPicker.js
@@ -0,0 +1,80 @@
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+  FlatList,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import defaultStyle from "../configs/styles";
+import AppText from "./AppText";
+import Screen from "./Screen";
+import PickerItem from "./PickerItem";
+
+const AppPicker = ({ icon, placeholder, selectedItem, onPress }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={defaultStyle.colors.medium}
+              style={styles.icon}
+            />
+          )}
+
+          <AppText style={styles.text}>
+            {placeholder}
+            {/* {selectedItem ? selectedItem.label : placeholder} */}
+          </AppText>
+
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyle.colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={selectedItem}
+            keyExtractor={(item) => item.value.toString()}
+            renderItem={({ item }) => (
+              <PickerItem
+                onPress={() => console.log(item)}
+                label={item.label}
+              />
+            )}
+          />
+        </Screen>
+      </Modal>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: defaultStyle.colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  icon: {
+    marginRight: 10,
+  },
+  text: {
+    flex: 1,
+  },
+});
+
+export default AppPicker;
