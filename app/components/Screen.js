7dfefbf5ba3230be8553aeacd713e40f49bae565
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-07 03:09:25 +0300

    feat: enhance Screen component with GestureHandler and SafeAreaProvider

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,11 +1,17 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 function Screen({ children, style }) {
   return (
-    <SafeAreaView style={[styles.screen, style]} edges={["top", "bottom"]}>
-      {children}
-    </SafeAreaView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <SafeAreaView style={[styles.screen, style]} edges={["top", "bottom"]}>
+          {children}
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
