78f5a9b5ad1488366737545bf0f74c7256347fd3
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-07 05:29:08 +0300

    feat: add ErrorMessage component for displaying error messages

---
+++ app/components/ErrorMessage.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import AppText from "./AppText";
+
+function ErrorMessage({ error, visible }) {
+  if (!visible || !error) return null;
+
+  return <AppText style={styles.error}>{error}</AppText>;
+}
+
+const styles = StyleSheet.create({
+  error: {
+    color: "red",
+    marginBottom: 10,
+  },
+});
+
+export default ErrorMessage;
