6dc335b9ed89b8dd7f3cef10e6cede157e09f3bf
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-07 05:04:09 +0300

    feat: enhance AppTextInput component with right icon support and improved styling

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,10 +1,9 @@
-import { View, TextInput, StyleSheet, Platform } from "react-native";
+import { View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
-
 import defaultStyle from "../configs/styles";
 
-const AppTextInput = ({ icon, ...otherProps }) => {
+const AppTextInput = ({ icon, rightIcon, onRightIconPress, ...otherProps }) => {
   return (
     <View style={styles.container}>
       {icon && (
@@ -15,7 +14,23 @@ const AppTextInput = ({ icon, ...otherProps }) => {
           style={styles.icon}
         />
       )}
-      <TextInput style={defaultStyle.text} {...otherProps} />
+
+      <TextInput
+        style={[defaultStyle.text, styles.textInput]}
+        placeholderTextColor={defaultStyle.colors.medium}
+        {...otherProps}
+      />
+
+      {rightIcon && (
+        <TouchableOpacity onPress={onRightIconPress}>
+          <MaterialCommunityIcons
+            name={rightIcon}
+            size={20}
+            color={defaultStyle.colors.medium}
+            style={styles.rightIcon}
+          />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
@@ -23,15 +38,21 @@ const AppTextInput = ({ icon, ...otherProps }) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: defaultStyle.colors.light,
-    flexDirection: "row",
     borderRadius: 25,
+    flexDirection: "row",
     width: "100%",
     padding: 15,
-    alignItems: "center",
     marginVertical: 10,
+    alignItems: "center",
   },
   icon: {
     marginRight: 10,
+  },
+  textInput: {
+    flex: 1,
+  },
+  rightIcon: {
+    marginLeft: 10,
   },
 });
 
