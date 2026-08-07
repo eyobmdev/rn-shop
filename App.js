4c3b45b46342948be9a60de424d02bd58398b7cf
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-08 02:33:03 +0300

    feat: create AppSubmitButton component to handle form submissions

--- App.js
+++ App.js
@@ -1,31 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { StyleSheet, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import AppTextInput from "./app/components/AppTextInput";
-import Screen from "./app/components/Screen";
-import AppPicker from "./app/components/AppPicker";
-import { useState } from "react";
-
+import LoginScreen from "./app/screens/LoginScreen";
 export default function App() {
-  const categories = [
-    { label: "Furniture", value: 1 },
-    { label: "Clothing", value: 2 },
-    { label: "Cameras", value: 3 },
-    { label: "Food", value: 4 },
-  ];
-  const [category, setCategory] = useState();
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        placeholder="Category"
-        icon="apps"
-      />
-      <AppTextInput placeholder="Email" icon="email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
