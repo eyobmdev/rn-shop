cf719af7e1902619fbaf45610daab8cced6dd20f
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-09 05:52:17 +0300

    e

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
+import LoginScreen from "./app/screens/LoginScreen";
 export default function App() {
-  return <ListingEditScreen />;
+  return <RegisterScreen />;
 }
