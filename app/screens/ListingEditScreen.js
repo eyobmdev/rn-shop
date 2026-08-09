9ea9a8a3f3c31b5ac8374d7de1be445f9ad2b86b
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-09 04:57:35 +0300

    feat: add ListingEditScreen for creating and editing listings

---
+++ app/screens/ListingEditScreen.js
@@ -0,0 +1,59 @@
+import { View, Text } from "react-native";
+import React from "react";
+import * as Yub from "yup";
+import Screen from "../components/Screen";
+
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+
+const validationSchema = Yub.object().shape({
+  title: Yub.string().required().min(3).label("Title"),
+  price: Yub.number().required().min(1).max(10000).label("Price"),
+  description: Yub.string().label("Description"),
+  category: Yub.object().required().nullable().label("Category"),
+});
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Camera", value: 3 },
+];
+
+const ListingEditScreen = () => {
+  return (
+    <Screen>
+      <AppForm
+        initialValues={{
+          title: "",
+          price: "",
+          description: "",
+          category: null,
+        }}
+        onSubmit={(value) => console.log(value)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField name="title" maxLength={255} placeholder="Title" />
+        <AppFormField
+          keyboardType="numeric"
+          maxLength={8}
+          name="price"
+          placeholder="Price"
+        />
+        <AppFormField
+          name="category"
+          items={categories}
+          placeholder="Category"
+        />
+        <AppFormField
+          name="description"
+          maxLength={255}
+          multiline
+          numberOfLines={3}
+          placeholder="Description"
+        />
+        <SubmitButton title="Post" />
+      </AppForm>
+    </Screen>
+  );
+};
+
+export default ListingEditScreen;
