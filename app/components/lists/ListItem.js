134857084631a3b103a7a1ebf151281355f0ba5f
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-09 05:23:49 +0300

    feat: add ListItem, ListItemDeleteAction, and ListItemSeparator components

---
+++ app/components/lists/ListItem.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { View, StyleSheet, Image, Pressable } from "react-native";
+import AppText from "../AppText";
+import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
+import colors from "../../configs/colors";
+
+function ListItem({
+  image,
+  IconComponent,
+  title,
+  subtitle,
+  onPress,
+  renderRightActions,
+}) {
+  return (
+    <Swipeable renderRightActions={renderRightActions}>
+      <Pressable
+        onPress={onPress}
+        style={({ pressed }) => [
+          styles.container,
+          {
+            backgroundColor: pressed ? "#ddd" : "#fff",
+          },
+        ]}
+      >
+        {IconComponent}
+        {image && <Image style={styles.image} source={image} />}
+
+        <View style={styles.detailContainer}>
+          <AppText style={styles.title}>{title}</AppText>
+          {subtitle && <AppText style={styles.subtitle}>{subtitle}</AppText>}
+        </View>
+      </Pressable>
+    </Swipeable>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  subtitle: {
+    color: colors.medium,
+  },
+  detailContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
+  press: {},
+});
+
+export default ListItem;
---
+++ app/components/lists/ListItemDeleteAction.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../../configs/colors";
+
+function ListItemDeleteAction({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <MaterialCommunityIcons name="trash-can" size={35} color={colors.white} />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.danger,
+    width: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default ListItemDeleteAction;
