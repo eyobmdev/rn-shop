4447cf4cd7923f484f3d33b90456bb16226c77f1
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-10 15:55:59 +0300

    feat: implement CategoryPickerItem component with icon and label rendering

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,8 +1,32 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import Icon from "./Icon";
+import AppText from "./AppText";
 
-const CategoryPickerItem = () => {
-  return null;
+const CategoryPickerItem = ({ item, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
+      <AppText style={styles.label} numberOfLines={2}>
+        {item.label}
+      </AppText>
+    </TouchableOpacity>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: "33.33%",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+
+  label: {
+    marginTop: 8,
+    textAlign: "center",
+    fontSize: 13,
+    lineHeight: 16,
+  },
+});
 
 export default CategoryPickerItem;
