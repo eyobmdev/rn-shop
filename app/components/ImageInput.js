7ad3dda98e97766a08854a76d8cc2792837454e6
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-12 01:59:12 +0300

    feat: add ImageInput component for image selection and display

---
+++ app/components/ImageInput.js
@@ -0,0 +1,83 @@
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import React, { useEffect } from "react";
+import colors from "../configs/colors";
+
+const ImageInput = ({ imageUri, onChange }) => {
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert("Permission required", "Please allow access to your photos.");
+      return;
+    }
+  };
+
+  const handleImage = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert("Delete", "Are you sure you want to delete this image?", [
+        { text: "Yes", onPress: () => onChange(null) },
+        { text: "No" },
+      ]);
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ["images"],
+        allowsEditing: false,
+        quality: 0.5,
+      });
+
+      if (!result.canceled && result.assets?.length > 0) {
+        onChange(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.log("ImagePicker error:", error);
+    }
+  };
+  return (
+    <TouchableWithoutFeedback onPress={handleImage}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            name="camera"
+            size={40}
+            color={colors.medium}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default ImageInput;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100,
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.light,
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
