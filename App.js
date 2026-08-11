335d2ef95e94c079c01e7673f4b45f15c16c5923
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-11 08:38:48 +0300

    refactor: clean up imports in App.js by removing unused screens

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import RegisterScreen from "./app/screens/RegisterScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import ListingDetailScreen from "./app/screens/ListingDetailScreen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import MessageScreen from "./app/screens/MessageScreen";
-import ListItem from "./app/components/lists/ListItem";
+import * as ImagePicker from "expo-image-picker";
+import { useEffect } from "react";
+import Screen from "./app/components/Screen";
 
 export default function App() {
+  useEffect(() => {}, []);
   return <ListingEditScreen />;
 }
