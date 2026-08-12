2fa0b5d0e0e01c61cadcf4ff8e8fbf14eb570742
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-12 15:08:28 +0300

    refactor: enhance layout and scrolling behavior in ImageInputList component

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,32 +1,43 @@
-import { StyleSheet, View } from "react-native";
 import React, { useRef } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
 import ImageInput from "./ImageInput";
-import { ScrollView } from "react-native";
 
 const ImageInputList = ({ imageUris = [], onRemoveImage, onAddImage }) => {
-  const scrollView = useRef();
+  const scrollView = useRef(null);
+
   return (
-    <ScrollView
-      ref={scrollView}
-      horizontal
-      onContentSizeChange={() => scrollView.current.scrollToEnd()}
-    >
-      <View style={styles.container}>
-        {imageUris.map((uri) => (
-          <ImageInput
-            imageUri={uri}
-            key={uri}
-            onChange={() => onRemoveImage(uri)}
-          />
-        ))}
-        <ImageInput onChange={(uri) => onAddImage(uri)} />
-      </View>
-    </ScrollView>
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onContentSizeChange={() => {
+          scrollView.current?.scrollToEnd({ animated: true });
+        }}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.imageWrapper}>
+              <ImageInput imageUri={uri} onChange={() => onRemoveImage(uri)} />
+            </View>
+          ))}
+          <View style={styles.imageWrapper}>
+            <ImageInput onChange={(uri) => onAddImage(uri)} />
+          </View>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
 export default ImageInputList;
 
 const styles = StyleSheet.create({
-  container: { flexDirection: "row", gap: 10 },
+  container: {
+    flexDirection: "row",
+    gap: 10,
+    paddingHorizontal: 5,
+    padding: 0,
+  },
+  imageWrapper: {},
 });
