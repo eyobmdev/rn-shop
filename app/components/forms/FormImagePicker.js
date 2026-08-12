bcd83e57fbe4a68cec96b2e3ef64a9a72f0d98db
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-12 15:46:12 +0300

    refactor: adjust function declaration style in FormImagePicker component

--- app/components/forms/FormImagePicker.js
+++ app/components/forms/FormImagePicker.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { useFormikContext } from "formik";
 
-import ErrorMessage from "./ErrorMessage";
 import ImageInputList from "../ImageInputList";
+import ErrorMessage from "./ErrorMessage";
 
-function FormImagePicker({ name }) {
-  const { errors, setFieldValue, touched, values } = useFormikContext();
+const FormImagePicker = ({ name }) => {
+  const { setFieldValue, errors, touched, values } = useFormikContext();
   const imageUris = values[name];
 
   const handleAdd = (uri) => {
     setFieldValue(name, [...imageUris, uri]);
   };
-
   const handleRemove = (uri) => {
     setFieldValue(
       name,
@@ -29,6 +28,6 @@ function FormImagePicker({ name }) {
       <ErrorMessage error={errors[name]} visible={touched[name]} />
     </>
   );
-}
+};
 
 export default FormImagePicker;
