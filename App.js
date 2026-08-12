d3a145d38be480f362ad2ffc79342a8ced3306b3
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-12 16:08:10 +0300

    refactor: simplify App component by removing image handling logic

--- App.js
+++ App.js
@@ -1,26 +1,6 @@
-import * as ImagePicker from "expo-image-picker";
-import { useEffect, useState } from "react";
-import Screen from "./app/components/Screen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
+import React from "react";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
-  return (
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
