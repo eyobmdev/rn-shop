c0f315b83cb5b84de2072c1d5b5256fb0b0b2a57
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-12 17:12:02 +0300

    refactor: replace location fetching logic with custom hook in ListingEditScreen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import * as Yub from "yup";
-import * as Location from "expo-location";
 
 import Screen from "../components/Screen";
 
@@ -12,6 +11,7 @@ import {
 } from "../components/forms";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/forms/FormImagePicker";
+import useLocation from "../hooks/useLocation";
 
 const validationSchema = Yub.object().shape({
   title: Yub.string().required().min(3).label("Title"),
@@ -79,38 +79,7 @@ const categories = [
 ];
 
 const ListingEditScreen = () => {
-const [location, setLocation] = useState(null);
-
-const getLocation = async () => {
-  try {
-    const { status } = await Location.requestForegroundPermissionsAsync();
-
-    if (status !== "granted") {
-      console.log("Location permission denied");
-      return;
-    }
-
-    const position = await Location.getCurrentPositionAsync({
-      accuracy: Location.Accuracy.Balanced,
-    });
-
-
-    if (!position) {
-      console.log("Could not get location");
-      return;
-    }
-
-    const { latitude, longitude } = position.coords;
-    setLocation({ latitude, longitude });
-
-  } catch (error) {
-    console.log("Error getting location:", error);
-  }
-};
-
-useEffect(() => {
-  getLocation();
-}, []);
+  const location = useLocation();
   return (
     <Screen>
       <AppForm
