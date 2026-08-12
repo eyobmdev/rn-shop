70eb4bcb93e958a71dc5674cb10c48c56fee16f7
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-12 04:58:40 +0300

    feat: implement image selection functionality with ImageInputList component

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import * as ImagePicker from "expo-image-picker";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Screen from "./app/components/Screen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
-  useEffect(() => {}, []);
-  return <ListingEditScreen />;
+  const [imageUris, setImageUris] = useState([]);
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  };
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+  };
+
+  return (
+    <Screen>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+    </Screen>
+  );
 }
