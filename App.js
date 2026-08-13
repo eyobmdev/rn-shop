6cf9d37946c1b6c7f986915c6d028ba72e48557d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-13 23:48:40 +0300

    feat: implement Tweet and TweetDetails screens with navigation support

--- App.js
+++ App.js
@@ -1,6 +1,45 @@
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { Button, Text } from "react-native";
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Screen from "./app/components/Screen";
+
+const Tweet = ({ navigation }) => {
+  return (
+    <Screen>
+      <Text>Tweet</Text>
+      <Button
+        title="View Tweet"
+        onPress={() => navigation.navigate("TweetDetails")}
+      />
+    </Screen>
+  );
+};
+
+const TweetDetails = () => {
+  return (
+    <Screen>
+      <Text>Tweet Details</Text>
+    </Screen>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweet" component={Tweet} />
+      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
