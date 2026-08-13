15f7765fdd4b71fc4299fba51fa01748e3dd79c0
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-14 02:10:12 +0300

    feat: add onPress functionality to Card component for better interactivity

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 import AppText from "./AppText";
 import colors from "../configs/colors";
 
-function Card({ title, subtitle, image }) {
+function Card({ title, subtitle, image, onPress }) {
   return (
-    <View style={styles.card}>
-      <Image source={image} style={styles.image} />
-      <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subtitle}>{subtitle}</AppText>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.card}>
+        <Image source={image} style={styles.image} />
+        <View style={styles.detailsContainer}>
+          <AppText style={styles.title}>{title}</AppText>
+          <AppText style={styles.subtitle}>{subtitle}</AppText>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
