a974094a7cf1a68f5431fdc164a4da2358902bd7
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-14 02:54:44 +0300

    feat: enhance AppNavigator with tabBar icons and custom button for ListingEdit

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import AccountScreen from "../screens/AccountScreen";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import NewListButton from "./NewListButton";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons size={size} color={color} name="home" />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="ListingEdit"
+      component={ListingEditScreen}
+      options={({ navigation }) => ({
+        tabBarButton: () => (
+          <NewListButton onPress={() => navigation.navigate("ListingEdit")} />
+        ),
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons
+            size={size}
+            color={color}
+            name="plus-circle"
+          />
+        ),
+      })}
+    />
+    <Tab.Screen
+      name="Account"
+      component={AccountNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons size={size} color={color} name="account" />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
