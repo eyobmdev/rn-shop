12c4e28932d82a73fb149a32a260743516bd0316
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-18 01:58:43 +0300

    fix: correct loading state management in loadListings function

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -1,35 +1,42 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, FlatList, Text } from "react-native";
 import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
-import colors from "../configs/colors";
 import listingsApi from "../api/listings";
 import routes from "../navigation/routes";
 import Screen from "../components/Screen";
 import AppText from "../components/AppText/AppText";
 import AppButton from "../components/AppButton";
+import ActivityIndicator from "../components/ActivityIndicator";
 
 const ListingScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, seLoading] = useState(false);
+
   useEffect(() => {
     loadListings();
   }, []);
 
   const loadListings = async () => {
+    seLoading(true);
     const response = await listingsApi.getListings();
+    seLoading(false);
+
     if (!response.ok) return setError(true);
+
     setError(false);
     setListings(response.data);
   };
 
   return (
-    <Screen style={styles.screen}>
+    <Screen>
       {error && (
         <>
           <AppText>Couldn't retrieve the listings.</AppText>
           <AppButton title="Retry" onPress={loadListings} />
         </>
       )}
+      <ActivityIndicator visible={loading} />
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -47,7 +54,3 @@ const ListingScreen = ({ navigation }) => {
 };
 
 export default ListingScreen;
-
-const styles = StyleSheet.create({
-  screen: {},
-});
