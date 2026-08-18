321628ca8e900c013c17cc14c39eda072f00899c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-18 22:55:55 +0300

    fix: correct component imports for Listings and ListingDetails in FeedNavigator

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import ListingScreen from "../screens/ListingScreen";
-import ListingDetailScreen from "../screens/ListingDetailScreen";
+import ListingsScreen from "../screens/ListingsScreen";
+import ListingDetailsScreen from "../screens/ListingDetailsScreen";
 
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => (
   <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="Listings" component={ListingScreen} />
-    <Stack.Screen name="ListingDetails" component={ListingDetailScreen} />
+    <Stack.Screen name="Listings" component={ListingsScreen} />
+    <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
   </Stack.Navigator>
 );
 
