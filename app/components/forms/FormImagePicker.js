329486417d4a23f3d5250e0b5535ce9898466c04
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-18 22:54:48 +0300

    refactor: change FormImagePicker to use regular function declaration and reorder imports

--- app/components/forms/FormImagePicker.js
+++ app/components/forms/FormImagePicker.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { useFormikContext } from "formik";
 
-import ImageInputList from "../ImageInputList";
 import ErrorMessage from "./ErrorMessage";
+import ImageInputList from "../ImageInputList";
 
-const FormImagePicker = ({ name }) => {
-  const { setFieldValue, errors, touched, values } = useFormikContext();
+function FormImagePicker({ name }) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
   const imageUris = values[name];
 
   const handleAdd = (uri) => {
     setFieldValue(name, [...imageUris, uri]);
   };
+
   const handleRemove = (uri) => {
     setFieldValue(
       name,
-      imageUris.filter((imageUri) => imageUri !== uri),
+      imageUris.filter((imageUri) => imageUri !== uri)
     );
   };
 
@@ -28,6 +29,6 @@ const FormImagePicker = ({ name }) => {
       <ErrorMessage error={errors[name]} visible={touched[name]} />
     </>
   );
-};
+}
 
 export default FormImagePicker;
