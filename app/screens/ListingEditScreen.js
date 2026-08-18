dd6acadd69012908ed2aba0ba98985b6ea0a0251
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-18 22:56:24 +0300

    refactor: update ListingEditScreen to use new form components and improve validation schema

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,101 +1,114 @@
-import React from "react";
-import * as Yub from "yup";
-
-import listingApi from "../api/listings";
-import Screen from "../components/Screen";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import * as Yup from "yup";
 
 import {
-  AppForm,
-  AppFormField,
+  Form,
+  FormField,
+  FormPicker as Picker,
   SubmitButton,
-  AppFormPicker,
 } from "../components/forms";
 import CategoryPickerItem from "../components/CategoryPickerItem";
+import Screen from "../components/Screen";
 import FormImagePicker from "../components/forms/FormImagePicker";
+import listingsApi from "../api/listings";
 import useLocation from "../hooks/useLocation";
+import UploadScreen from "./UploadScreen";
 
-const validationSchema = Yub.object().shape({
-  title: Yub.string().required().min(3).label("Title"),
-  price: Yub.number().required().min(1).max(10000).label("Price"),
-  description: Yub.string().label("Description"),
-  category: Yub.object().required().nullable().label("Category"),
-  images: Yub.array().min(1, "Please select at least one image."),
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(1).label("Title"),
+  price: Yup.number().required().min(1).max(10000).label("Price"),
+  description: Yup.string().label("Description"),
+  category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image."),
 });
 
 const categories = [
   {
+    backgroundColor: "#fc5c65",
+    icon: "floor-lamp",
     label: "Furniture",
     value: 1,
-    backgroundColor: "#fc5c65",
-    icon: "table-furniture",
   },
   {
-    label: "Clothing",
-    value: 2,
     backgroundColor: "#fd9644",
-    icon: "tshirt-crew",
-  },
-  {
-    label: "Electronics",
-    value: 3,
-    backgroundColor: "#fed330",
-    icon: "laptop",
-  },
-  {
-    label: "Books",
-    value: 4,
-    backgroundColor: "#26de81",
-    icon: "book-open-variant",
-  },
-  {
-    label: "Sports",
-    value: 5,
-    backgroundColor: "#2bcbba",
-    icon: "basketball",
-  },
-  {
-    label: "Beauty",
-    value: 6,
-    backgroundColor: "#45aaf2",
-    icon: "face-woman",
-  },
-  {
-    label: "Toys",
-    value: 7,
-    backgroundColor: "#4b7bec",
-    icon: "puzzle",
-  },
-  {
-    label: "Automotive",
-    value: 8,
-    backgroundColor: "#a55eea",
     icon: "car",
+    label: "Cars",
+    value: 2,
   },
   {
-    label: "Others",
-    value: 9,
+    backgroundColor: "#fed330",
+    icon: "camera",
+    label: "Cameras",
+    value: 3,
+  },
+  {
+    backgroundColor: "#26de81",
+    icon: "cards",
+    label: "Games",
+    value: 4,
+  },
+  {
+    backgroundColor: "#2bcbba",
+    icon: "shoe-heel",
+    label: "Clothing",
+    value: 5,
+  },
+  {
+    backgroundColor: "#45aaf2",
+    icon: "basketball",
+    label: "Sports",
+    value: 6,
+  },
+  {
+    backgroundColor: "#4b7bec",
+    icon: "headphones",
+    label: "Movies & Music",
+    value: 7,
+  },
+  {
+    backgroundColor: "#a55eea",
+    icon: "book-open-variant",
+    label: "Books",
+    value: 8,
+  },
+  {
     backgroundColor: "#778ca3",
-    icon: "apps",
+    icon: "application",
+    label: "Other",
+    value: 9,
   },
 ];
 
-const ListingEditScreen = () => {
+function ListingEditScreen() {
   const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing) => {
-    const result = await listingApi.addListing(
+  const handleSubmit = async (listing, { resetForm }) => {
+    setProgress(0);
+    setUploadVisible(true);
+    const result = await listingsApi.addListing(
       { ...listing, location },
-      (progress) => console.log(progress), 
+      (progress) => setProgress(progress)
     );
+
     if (!result.ok) {
+      setUploadVisible(false);
       return alert("Could not save the listing");
     }
 
-    alert("Success");
+    resetForm();
   };
+
   return (
-    <Screen>
-      <AppForm
+    <Screen style={styles.container}>
+      <UploadScreen
+        onDone={() => setUploadVisible(false)}
+        progress={progress}
+        visible={uploadVisible}
+      />
+      <Form
         initialValues={{
           title: "",
           price: "",
@@ -107,16 +120,15 @@ const ListingEditScreen = () => {
         validationSchema={validationSchema}
       >
         <FormImagePicker name="images" />
-
-        <AppFormField name="title" maxLength={255} placeholder="Title" />
-        <AppFormField
+        <FormField maxLength={255} name="title" placeholder="Title" />
+        <FormField
           keyboardType="numeric"
           maxLength={8}
           name="price"
           placeholder="Price"
           width={120}
         />
-        <AppFormPicker
+        <Picker
           items={categories}
           name="category"
           numberOfColumns={3}
@@ -124,17 +136,22 @@ const ListingEditScreen = () => {
           placeholder="Category"
           width="50%"
         />
-        <AppFormField
-          name="description"
+        <FormField
           maxLength={255}
           multiline
+          name="description"
           numberOfLines={3}
           placeholder="Description"
         />
         <SubmitButton title="Post" />
-      </AppForm>
+      </Form>
     </Screen>
   );
-};
+}
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
 export default ListingEditScreen;
