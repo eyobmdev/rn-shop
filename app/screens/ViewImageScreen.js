5c79fbed75c277e2316b6f980399d99a0d42657e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-18 22:56:51 +0300

    refactor: update ViewImageScreen for improved icon sizes and style adjustments

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,49 +1,49 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import colors from "../configs/colors";
+import colors from "../config/colors";
 
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
       <View style={styles.closeIcon}>
-        <MaterialCommunityIcons name="close" color="white" size={30} />
+        <MaterialCommunityIcons name="close" color="white" size={35} />
       </View>
       <View style={styles.deleteIcon}>
         <MaterialCommunityIcons
           name="trash-can-outline"
           color="white"
-          size={30}
+          size={35}
         />
       </View>
       <Image
         resizeMode="contain"
-        source={require("../assets/chair.jpg")}
         style={styles.image}
+        source={require("../assets/chair.jpg")}
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: "100%",
-    height: "100%",
-  },
   closeIcon: {
     position: "absolute",
-    top: 50,
+    top: 40,
     left: 30,
   },
   container: {
-    flex: 1,
     backgroundColor: colors.black,
+    flex: 1,
   },
   deleteIcon: {
     position: "absolute",
-    top: 50,
+    top: 40,
     right: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
