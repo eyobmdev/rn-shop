d2933bb0caa48d88f65c10ea9fff32841c75c37e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-19 00:20:02 +0300

    refactor: wrap NavigationContainer in SafeAreaProvider for improved layout handling

--- App.js
+++ App.js
@@ -1,93 +1,16 @@
-import { Button, Text } from "react-native";
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
 
-import Screen from "./app/components/Screen";
-import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 
-const Link = () => {
-  const navigation = useNavigation();
-  return (
-    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
-  );
-};
-
-const Tweets = ({ navigation }) => {
-  return (
-    <Screen>
-      <Text>Tweet</Text>
-      <Button
-        title="View Tweet"
-        onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-      />
-      <Link />
-    </Screen>
-  );
-};
-
-const TweetDetails = ({ route }) => {
-  return (
-    <Screen>
-      <Text>Tweet Details {route.params.id}</Text>
-    </Screen>
-  );
-};
-
-const Stack = createStackNavigator();
-
-const StackNavigator = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Tweet" component={Tweets} />
-      <Stack.Screen
-        options={({ route }) => ({ title: route.params.id })}
-        name="TweetDetails"
-        component={TweetDetails}
-      />
-    </Stack.Navigator>
-  );
-};
-
-const Accounts = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator
-      tabBarOptions={{
-        activeBackgroundColor: "tomato",
-        activeTintColor: "white",
-        inactiveBackgroundColor: "#eee",
-        inactiveTintColor: "black",
-      }}
-    >
-      <Tab.Screen
-        name="Feed"
-        component={StackNavigator}
-        options={{
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons name="home" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen name="Accounts" component={Accounts} />
-    </Tab.Navigator>
-  );
-};
-
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
