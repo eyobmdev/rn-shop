dbf5f9b0e3fd997771e2a78cabdbe08216eff1e5
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-18 22:53:27 +0300

    refactor: simplify ActivityIndicator component by removing unnecessary View wrapper

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,18 +1,15 @@
-import LottieView from "lottie-react-native";
 import React from "react";
-import { View } from "react-native";
+import LottieView from "lottie-react-native";
 
 function ActivityIndicator({ visible = false }) {
   if (!visible) return null;
 
   return (
-      <LottieView
-        autoPlay
-        loop
-        source={require("../assets/animations/loader.json")}
-        style={{ width: "100%", height: "100%" }}
-        
-      />
+    <LottieView
+      autoPlay
+      loop
+      source={require("../assets/animations/loader.json")}
+    />
   );
 }
 
