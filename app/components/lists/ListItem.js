d108b422a6140322dd98d2058a18192f00027098
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-18 22:55:08 +0300

    refactor: update ListItem component to use TouchableHighlight and improve styling

--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -1,75 +1,69 @@
 import React from "react";
-import { View, StyleSheet, Image, Pressable } from "react-native";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
-import AppText from "../AppText";
-import colors from "../../configs/colors";
+import Text from "../Text";
+import colors from "../../config/colors";
 
 function ListItem({
+  title,
+  subTitle,
   image,
   IconComponent,
-  title,
-  subtitle,
   onPress,
   renderRightActions,
 }) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
-      <Pressable
-        onPress={onPress}
-        style={({ pressed }) => [
-          styles.container,
-          {
-            backgroundColor: pressed ? "#ddd" : "#fff",
-          },
-        ]}
-      >
-        {IconComponent}
-        {image && <Image style={styles.image} source={image} />}
-
-        <View style={styles.detailContainer}>
-          <AppText style={styles.title} numberOfLines={1}>
-            {title}
-          </AppText>
-          {subtitle && (
-            <AppText style={styles.subtitle} numberOfLines={1}>
-              {subtitle}
-            </AppText>
-          )}
+      <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+        <View style={styles.container}>
+          {IconComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
+            <Text style={styles.title} numberOfLines={1}>
+              {title}
+            </Text>
+            {subTitle && (
+              <Text style={styles.subTitle} numberOfLines={2}>
+                {subTitle}
+              </Text>
+            )}
+          </View>
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name="chevron-right"
+            size={25}
+          />
         </View>
-        <MaterialCommunityIcons
-          color={colors.medium}
-          name="chevron-right"
-          size={25}
-        />
-      </Pressable>
+      </TouchableHighlight>
     </Swipeable>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    padding: 10,
     alignItems: "center",
+    flexDirection: "row",
+    padding: 15,
+    backgroundColor: colors.white,
+  },
+  detailsContainer: {
+    flex: 1,
+    marginLeft: 10,
+    justifyContent: "center",
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
   },
-  title: {
-    fontWeight: "bold",
-  },
-  subtitle: {
+  subTitle: {
     color: colors.medium,
   },
-  detailContainer: {
-    marginLeft: 10,
-    justifyContent: "center",
-    flex: 1,
+  title: {
+    fontWeight: "500",
   },
-  press: {},
 });
 
 export default ListItem;
