698f35fc4c4e9dbd9f0cf03fbe7a3adec3530376
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-18 22:53:51 +0300

    refactor: remove unused StyleSheet import and clean up Icon component

--- app/components/Icon.js
+++ app/components/Icon.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function Icon({
@@ -19,7 +19,7 @@ function Icon({
         alignItems: "center",
       }}
     >
-      <MaterialCommunityIcons name={name} size={size * 0.5} color={iconColor} />
+      <MaterialCommunityIcons name={name} color={iconColor} size={size * 0.5} />
     </View>
   );
 }
