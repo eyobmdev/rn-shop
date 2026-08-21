52f98679c9486f80ea68f36590cdd1dbf43efb73
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-22 01:39:36 +0300

    feat: enhance demo function to store and retrieve additional data in AsyncStorage; add new cache utility file

--- App.js
+++ App.js
@@ -9,9 +9,13 @@ import AppNavigator from "./app/navigation/AppNavigator";
 async function demo() {
   try {
     await AsyncStorage.setItem("userToken", "abc123");
+    await AsyncStorage.setItem("jj", JSON.stringify({id:4}));
 
     const token = await AsyncStorage.getItem("userToken");
     console.log("Stored token:", token);
+
+    const jj = await AsyncStorage.getItem("jj");
+    console.log("Stored jj:", jj);
 
     await AsyncStorage.removeItem("userToken");
   } catch (error) {
