fafc40c9bf104dc2cf62aa463a4a97c9446fe6a8
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-22 02:02:03 +0300

    feat: add caching utility to store data with timestamp in AsyncStorage

--- app/utility/cache.js
+++ app/utility/cache.js
@@ -1,1 +1,19 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const prefix = "cache";
+
+const store = async (key, value) => {
+  try {
+    const item = {
+      value: value,
+      timestamp: Date.now(),
+    };
+    await AsyncStorage.setItem(prefix + key, JSON.stringify(item));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default {
+  store,
+};
