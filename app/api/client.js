a84c2472d234220b964b307e75b7db42e357f606
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-26 15:38:38 +0300

    feat: enhance API client by integrating caching functionality in the get method

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,14 @@
 import { create } from "apisauce";
+import cache from "../utility/cache";
 
 const client = create({
   baseURL: "http://127.0.0.1:9000/api",
 });
+
+const get = client.get;
+
+client.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig);
+};
 
 export default client;
