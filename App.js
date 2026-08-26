7359d6a01dd8aab9aaae3698667ce2d1cdb2c6e7
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-27 00:07:33 +0300

    feat: integrate AuthContext for user authentication state management

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -6,16 +6,18 @@ import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import AuthContext from "./app/auth/context";
 
 export default function App() {
+  const [user, setUser] = useState();
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <SafeAreaProvider>
         <NavigationContainer theme={navigationTheme}>
-          <AuthNavigator />
+          {user ? <AppNavigator /> : <AuthNavigator />}
         </NavigationContainer>
       </SafeAreaProvider>
-    </>
+    </AuthContext.Provider>
   );
 }
