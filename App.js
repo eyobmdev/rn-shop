94e5529d7c7380f4b9202b35c157823f09769e93
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-26 20:45:08 +0300

    feat: add OfflineNotice component to display internet connectivity status

--- App.js
+++ App.js
@@ -1,38 +1,20 @@
 import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
-
-async function demo() {
-  try {
-    await AsyncStorage.setItem("userToken", "abc123");
-    await AsyncStorage.setItem("jj", JSON.stringify({id:4}));
- 
-    const token = await AsyncStorage.getItem("userToken");
-    console.log("Stored token:", token);
-
-    const jj = await AsyncStorage.getItem("jj");
-    console.log("Stored jj:", jj);
-
-    await AsyncStorage.removeItem("userToken");
-  } catch (error) {
-    console.error("AsyncStorage error:", error);
-  }
-}
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
-  useEffect(() => {
-    demo();
-  }, []);
-
   return (
-    <SafeAreaProvider>
-      <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <>
+      <OfflineNotice />
+      <SafeAreaProvider>
+        <NavigationContainer theme={navigationTheme}>
+          <AppNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </>
   );
 }
