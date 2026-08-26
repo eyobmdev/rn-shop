65d45ce202224a2b74a031dafc9cdf11fd0eefd5
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-26 16:06:45 +0300

    feat: implement caching in API client get method to improve data retrieval

--- app/api/client.js
+++ app/api/client.js
@@ -9,6 +9,15 @@ const get = client.get;
 
 client.get = async (url, params, axiosConfig) => {
   const response = await get(url, params, axiosConfig);
+
+  if (response.ok) {
+    cache.store(url, response.data);
+    return response;
+  }
+
+  const data = await cache.get(url);
+
+  return data ? { ok: true, data } : response;
 };
 
 export default client;
