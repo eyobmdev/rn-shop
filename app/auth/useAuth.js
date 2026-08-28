e25fdf3f1b2dda9983d9695b49d12dc3b37a4399
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-29 00:42:36 +0300

    feat: add login function to useAuth hook for user authentication

--- app/auth/useAuth.js
+++ app/auth/useAuth.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { jwtDecode } from "jwt-decode";
 
 import authStorage from "./storage";
 import AuthContext from "./context";
@@ -6,12 +7,18 @@ import AuthContext from "./context";
 const useAuth = () => {
   const { user, setUser } = useContext(AuthContext);
 
+  const login = (authToken) => {
+    const user = jwtDecode(authToken);
+    setUser(user);
+    authStorage.storeToken(authToken);
+  };
+
   const logOut = () => {
     setUser(null);
     authStorage.removeToken();
   };
 
-  return { user, logOut };
+  return { user, login, logOut };
 };
 
 export default useAuth;
