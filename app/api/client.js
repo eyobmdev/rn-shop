f94c755b0399f2c7de57bb3935b5a5b1ccf3b441
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-29 00:53:04 +0300

    refactor: add async request transform to client for authentication token handling

--- app/api/client.js
+++ app/api/client.js
@@ -1,8 +1,15 @@
 import { create } from "apisauce";
 import cache from "../utility/cache";
+import authStorage from "../auth/storage";
 
 const client = create({
   baseURL: "http://127.0.0.1:9000/api",
+});
+
+client.addAsyncRequestTransform(async (request) => {
+  const authToken = await authStorage.getToken();
+  if (!authStorage) return;
+  request.headers["x-auth-token"] = authToken;
 });
 
 const get = client.get;
