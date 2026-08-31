841ad2d196bf289f55023d910cedc37b0c4938f9
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-08-31 23:07:27 +0300

    feat: add navigation reference to NavigationContainer in App.js

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import AuthNavigator from "./app/navigation/AuthNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
+import { navigationRef } from "./app/navigation/rootNavigation";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -43,7 +44,7 @@ export default function App() {
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <SafeAreaProvider>
-        <NavigationContainer theme={navigationTheme}>
+        <NavigationContainer ref={navigationRef} theme={navigationTheme}>
           {user ? <AppNavigator /> : <AuthNavigator />}
         </NavigationContainer>
       </SafeAreaProvider>
